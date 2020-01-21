05c6f3f3be4bfdc9320d2c88b7d3de1e35d54f76
Henry Quinn <henryquinniv@gmail.com>
2020-01-21 16:21:03 -0500

    Updating site description meta tag.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import layoutStyles from "./layout.module.css"
 export default ({ children }) => (
   <div className={layoutStyles.terminalWindow}>
     <Helmet>
-      <meta name="description" content="The resume site for Henry Edward Quinn IV" />
+      <meta name="description" content="The personal resume site for Henry Edward Quinn IV" />
       <meta name="keywords" content="Henry, Quinn, DevOps, Engineer, Resume, Docker, Bash, GoLang, Go, JavaScript, JS, Automation" />
       <meta charset="UTF-8" />
       <html lang="en" />
