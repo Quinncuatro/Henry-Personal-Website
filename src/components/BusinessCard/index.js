2bf9e7c271cad857b451bb39ee8fa67de613656e
Henry Quinn <henryquinniv@gmail.com>
2020-03-05 20:27:38 -0500

    Add DEV.to link.

--- src/components/BusinessCard/index.js
+++ src/components/BusinessCard/index.js
@@ -7,14 +7,15 @@ export default () => ( <div>
   <div>
     <h1>Henry Quinn IV</h1>
     <h3>Automation Engineer</h3>
-    ------------------------------------------------<br />
+    -------------------------------------------------------<br />
     <Link to="/blog/"> [ Blog ]</Link>
+    <a href="https://dev.to/quinncuatro" target="_blank" rel="noopener noreferrer"> [ DEV.to ]</a>
     <a href="https://github.com/quinncuatro" target="_blank" rel="noopener noreferrer"> [ GitHub ]</a>
     <a href="https://www.linkedin.com/in/henryquinniv" target="_blank" rel="noopener noreferrer"> [ LinkedIn ]</a>
     <a href="https://www.twitter.com/quinncuatro" target="_blank" rel="noopener noreferrer"> [ Twitter ]</a><br /><br />
     <a href={ "mailto:"+myEmail }><span>{ myEmail }</span></a> /
     <a href="https://docs.google.com/document/d/12AERamrFtIQdnkQcnZG7Bq9R2u3F0T70YYVrRLdr5ek/export?format=pdf" target="_blank" rel="noopener noreferrer"> Printer Friendly Resume</a><br />
-    ------------------------------------------------<br />
+    -------------------------------------------------------<br />
     </div>
   </div>
 )
