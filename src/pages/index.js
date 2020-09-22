22319063fe98a548f5d1d7afc44bb5bb03ecc728
Henry Quinn <hquinn@clarityssi.com>
2020-09-22 12:54:02 -0400

    Adding CSS back in for link color/hover.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import CurrentLogin from "../components/CurrentLogin"
+import styles from "./index.module.css"
 
 export default () => (
   <Layout>
