d6467666e30693b30e55f3e7f40f8916cb75654f
Henry Quinn <henryquinn@hey.com>
2021-02-04 00:06:15 -0500

    Adding custom 404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../components/Layout"
+import PageNotFoundLogin from "../components/PageNotFoundLogin"
+
+export default () => (
+  <Layout>
+    <PageNotFoundLogin />
+  </Layout>
+);
