0c22b4f996e2a2e0fc5e43fff469f5ed6e78e382
Henry Quinn <henryquinn@hey.com>
2021-02-09 01:06:57 -0500

    Streamlined menu after adding contact page.

--- src/components/BusinessCard/index.js
+++ src/components/BusinessCard/index.js
@@ -1,20 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-let myEmail = "HenryQuinnIV" + String.fromCharCode(64) + "gmail.com";
-
 export default () => ( <div>
   <div>
-    ------------------------------------------------<br />
-    <Link to="/"> [ Home ]</Link>
-    <Link to="/blog/"> [ Blog ]</Link>
-    <Link to="/resume/"> [ Resume ]</Link>
-    <Link to="/talks/"> [ Talks ]</Link><br /><br />
-    <a href={ "mailto:"+myEmail }> [ Email Me ]</a>
-    <a href="https://github.com/quinncuatro" target="_blank" rel="noopener noreferrer"> [ GitHub ]</a>
-    <a href="https://www.linkedin.com/in/henryquinniv" target="_blank" rel="noopener noreferrer"> [ LinkedIn ]</a>
-    <a href="https://www.twitter.com/quinncuatro" target="_blank" rel="noopener noreferrer"> [ Twitter ]</a><br />
-    ------------------------------------------------<br />
+    --------------------------------------------------
+    <br />
+    <Link to="/">[ Home ]</Link>&nbsp;
+    <Link to="/blog/">[ Blog ]</Link>&nbsp;
+    <Link to="/contact/">[ Contact ]</Link>&nbsp;
+    <Link to="/resume/">[ Resume ]</Link>&nbsp;
+    <Link to="/talks/">[ Talks ]</Link>
+    <br />
+    --------------------------------------------------<br />
     </div>
   </div>
 )
