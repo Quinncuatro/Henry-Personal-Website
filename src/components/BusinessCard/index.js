b45f8b542e37e722367d773c84112cc6a838900f
Henry Quinn <henryquinn@hey.com>
2021-02-08 22:55:48 -0500

    Updating menu for new resume page.

--- src/components/BusinessCard/index.js
+++ src/components/BusinessCard/index.js
@@ -5,16 +5,16 @@ let myEmail = "HenryQuinnIV" + String.fromCharCode(64) + "gmail.com";
 
 export default () => ( <div>
   <div>
-    -------------------------------------------------------<br />
+    ------------------------------------------------<br />
     <Link to="/"> [ Home ]</Link>
-    <Link to="/blog/"> [ Blog ]</Link> 
-    <Link to="/talks/"> [ Talks ]</Link> 
-    <a href="https://docs.google.com/document/d/12AERamrFtIQdnkQcnZG7Bq9R2u3F0T70YYVrRLdr5ek/export?format=pdf" target="_blank" rel="noopener noreferrer"> [ Printer Friendly Resume ]</a><br /><br />
+    <Link to="/blog/"> [ Blog ]</Link>
+    <Link to="/resume/"> [ Resume ]</Link>
+    <Link to="/talks/"> [ Talks ]</Link><br /><br />
     <a href={ "mailto:"+myEmail }> [ Email Me ]</a>
     <a href="https://github.com/quinncuatro" target="_blank" rel="noopener noreferrer"> [ GitHub ]</a>
     <a href="https://www.linkedin.com/in/henryquinniv" target="_blank" rel="noopener noreferrer"> [ LinkedIn ]</a>
     <a href="https://www.twitter.com/quinncuatro" target="_blank" rel="noopener noreferrer"> [ Twitter ]</a><br />
-    -------------------------------------------------------<br />
+    ------------------------------------------------<br />
     </div>
   </div>
 )
