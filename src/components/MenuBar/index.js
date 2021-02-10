2347f3302274cf3aa1ac72a73d7b12e362398e99
Henry Quinn <henryquinn@hey.com>
2021-02-10 00:51:37 -0500

    Renaming Home to Henry Quinn in MenuBar.

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -3,16 +3,16 @@ import { Link } from "gatsby"
 
 export default () => ( <div>
   <div>
-    ---------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------
     <br />
-    <Link to="/">[ Home ]</Link>&nbsp;
+    <Link to="/">[ Henry Quinn ]</Link>&nbsp;
     <Link to="/blog/">[ Blog ]</Link>&nbsp;
     <Link to="/contact/">[ Contact ]</Link>&nbsp;
     <Link to="/resume/">[ Resume ]</Link>&nbsp;
     <Link to="/signal-boost/">[ Signal Boost ]</Link>&nbsp;
     <Link to="/talks/">[ Talks // Pods ]</Link>
     <br />
-    ---------------------------------------------------------------------------<br />
+    ----------------------------------------------------------------------------------<br />
     </div>
   </div>
 )
