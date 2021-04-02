308e605add66d2b579e1804e1f67d7c3b8871ca7
Henry Quinn <hquinn@clarityssi.com>
2021-04-02 16:06:29 -0400

    Adding comma to footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ let year = now.getFullYear();
 export default () => (
   <div>
     <hr />
-    <p>Made with <span className={footerStyles.heart}>&#10084;</span> in New Haven || &copy; {year} Henry Quinn</p>
+    <p>Made with <span className={footerStyles.heart}>&#10084;</span> in New Haven || &copy; {year}, Henry Quinn</p>
   </div>
 )
 
