c3b7a65b1a09eea923b02f5dd88d80f0364d1dde
Henry Quinn <hquinn@clarityssi.com>
2021-04-07 12:19:27 -0400

    Cleaning up the HomePage component.

--- drafts/nhv-rad-shit/index.md
+++ drafts/nhv-rad-shit/index.md
@@ -1,3 +1,7 @@
 https://www.theatlantic.com/health/archive/2021/01/pandemic-goodbye-casual-friends/617839/
 
 This article says a lot about the emotional side of why this needs to exist.
+
+https://mileslasater.com/
+
+This guy may be able to help.
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -47,26 +47,6 @@ const HomePage = () => {
           <h3>Please <Link to="/contact/">hit me up</Link> if you want to collab on something!</h3>
         </div>
         <hr/>
-        <div>
-          <h1>Here's a selection of my writing:</h1>
-          <strong className={homePageStyles.articleName}><Link to="/posts/ipfs-1-decentralized-web-is-here/">IPFS-The Decentralized Web Is Finally Here</Link></strong>
-          <h5>Category: Decentralized Web, IPFS || Date: January 27th, 2021</h5>
-          <p>It's the spring of 2011. I've already been accepted to college. Senioritis is setting in and 3OH!3 is blaring on my headphones as I sit at my desk at 2 AM on a school night... Homework long since done, I'm staring at the screen of the heavily used 17" Gateway laptop I managed to…</p>
-          <strong className={homePageStyles.articleName}><Link to="/posts/day3-fun-with-flags/">Learning DevOps In Public (Day 3) - Fun With Flags</Link></strong>
-          <h5>Category: DevOps || Date: July 4th, 2019</h5>
-          <p>First off, some housekeeping. I can already tell that coming up with a daily post is going to be harder than my buddy Alex had with his SVG work. He had one animation to complete every day. I'm spending all month working towards one larger goal. However, I'll do my best to share…</p>
-        </div>
-        <hr/>
-        <div>
-          <h1>And here are some things I've built:</h1>
-          <p><strong className={homePageStyles.projectName}><a href="https://github.com/Quinncuatro/service-relief-new-haven" target="_blank" rel="noopener noreferrer">New Haven Relief Resources</a></strong> (2020) - Built a web app to crowdsource and relay information regarding available aid in New Haven during the COVID-19 pandemic (including food banks, health/family resources, and business help). Includes a sister repository to help others set up a no-code version for their own city using only free resources.</p>
-          <p><i>JavaScript, React, GatsbyJS, AirTable, Netlify.</i></p>
-          <p><a href="https://newhavenreliefresources.com" target="_blank" rel="noopener noreferrer">[ Website ]</a>
-          </p>
-          <p><strong className={homePageStyles.projectName}><a href="https://github.com/Quinncuatro/SLAter" target="_blank" rel="noopener noreferrer">SLAter</a></strong> (2018) - Programmed a Raspberry Pi to monitor my DeskPro queue and alert me via external speakers when I have a new ticket assigned to me. This workflow was also used to remind a coworker to drink water and cure his gout.</p>
-          <p><i>JavaScript, Node, MySQL, Raspberry Pi.</i></p>
-        </div>
-        <hr/>
         <Footer />
       </div>
     </div>
